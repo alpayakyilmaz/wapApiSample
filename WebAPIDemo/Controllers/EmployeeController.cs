75a26676617914b283d63492975c645fd3e27adf
Alpay Akyilmaz <mehmet.alpay@erbul.com>
2017-06-29 14:25:52 +0300

    First Commit

---
+++ WebAPIDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+using System.Runtime.Serialization;
+
+
+namespace WebAPIDemo.Controllers
+{
+    [Serializable]  
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employees> Get()
+        
+        {
+          EmployeeDbEntities db = new EmployeeDbEntities();
+          return db.Employees.ToList();
+           
+        }
+
+        public Employees Get(int id)
+        {
+            EmployeeDbEntities db = new EmployeeDbEntities();
+            return db.Employees.FirstOrDefault(x => x.ID == id);
+
+        }
+
+        public HttpResponseMessage Post([FromBody] Employees employee)
+        {
+            try
+            {
+                using (EmployeeDbEntities db = new EmployeeDbEntities())
+                {
+                    db.Employees.Add(employee);
+                    db.SaveChanges();
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+       
+        }
+    }
+}
---
+++ WebAPIUnitTest/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebAPIDemo.Controllers;
+using EmployeeDataAccess;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+namespace WebAPIUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetAllEmployeeTest()
+        {
+            var testObject = GetAllEmployees();
+            var controller = new EmployeeController();
+
+            var result = controller.Get() as List<Employees>;
+            Assert.AreEqual(testObject.Count, result.Count);
+
+
+        }
+
+        private List<Employees> GetAllEmployees()
+        {
+            var testEmployee = new List<Employees>();
+            testEmployee.Add(new Employees { ID = 1, FirstName = "Test 1 Employee", LastName = "Test1 Employee Last Name", Gender = "Male", Salary = 1400 });
+            testEmployee.Add(new Employees { ID = 2, FirstName = "Test 2 Employee", LastName = "Test2 Employee Last Name", Gender = "Female", Salary = 1200 });
+            testEmployee.Add(new Employees { ID = 3, FirstName = "Test 3 Employee", LastName = "Test3 Employee Last Name", Gender = "Male", Salary = 900 });
+            testEmployee.Add(new Employees { ID = 4, FirstName = "Test 4 Employee", LastName = "Test4 Employee Last Name", Gender = "Female", Salary = 1200 });
+
+            return testEmployee;
+        }
+
+
+    }
+}
